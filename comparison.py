9f341c1d930829b3e85a5add2945a130040fac72
lucynjora <114142461+lucynjora@users.noreply.github.com>
2023-02-11 17:32:03 +0300

    Add files via upload

---
+++ comparison.py
@@ -0,0 +1,12 @@
+#program to find yhe largest of the three numbers
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+num3=int(input("enter the third number"))
+if (num1>num2 and num3):
+    print("num 1 is largest")
+if(num2>num1 and num2):
+    print("num2 is largest")
+if(num3>num1 and num2):
+    print("num3 is largest")
+    
+
